b75e0096119d26b874cf0ed41ad20db9ce98b0d4
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-16 12:39:19 -0400

    first commit

---
+++ learningCsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There once was a lady name Maddie");
+            Console.WriteLine("She was 60 years old");
+            Console.WriteLine("She really like to walk her dogs");
+            Console.WriteLine("But didnt like somebody who was also 60");
+
+            Console.ReadLine();
+        }
+    }
+}
