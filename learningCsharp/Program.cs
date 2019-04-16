01de51135241e647f118bfbc28f355a33b8b7e87
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-16 16:59:35 -0400

    methods added

--- learningCsharp/Program.cs
+++ learningCsharp/Program.cs
@@ -10,26 +10,16 @@ namespace learningCsharp
     {
         static void Main(string[] args)
         {
-            //must use [] to tell that its array of ints
-            int[] luckyNumbers = { 2, 5, 22, 56, 32, 69, 12, 5 };
-            //empty array for strings
-            //new string[number of values for the array to hold]
-            string[] friends = new string[5];
-            friends[0] = "Tom";
-            friends[1] = "Evan";
-            friends[2] = "Rob";
-            friends[3] = "Mike";
-            friends[4] = "Nate";
-
-
-            Console.WriteLine(luckyNumbers[5] + friends[3]);
-
+            //call methods here in main method to get output
+            //call the method
+            SayHi("Maddie", 69);
             Console.ReadLine();
         }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age);
+        }
+
     }
 }
-
-////Convert.Method() is used to convert things such as string to num
-//int num = Convert.ToInt32("12");
-//Console.WriteLine(num + 6);
-//Console.ReadLine();
