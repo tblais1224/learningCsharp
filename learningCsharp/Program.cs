89146da505c9db6c3e75fd952930c40df9997a91
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-16 16:12:36 -0400

    add calculator

--- learningCsharp/Program.cs
+++ learningCsharp/Program.cs
@@ -10,16 +10,22 @@ namespace learningCsharp
     {
         static void Main(string[] args)
         {
-            //writes the string to the console and stays on the same line
-            Console.Write("Enter your name: ");
-            //pauses program to allow user to input text
-            string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            string age = Console.ReadLine();
-            Console.WriteLine("Hello " + name + ", you are " + age + "years old.");
-            //pauses so we can view result
+            //writes to console
+            Console.Write("Enter a number: ");
+            //readline pauses script until user inputs, 
+            //result is converted from string to int32
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
             Console.ReadLine();
         }
     }
 }
 
+////Convert.Method() is used to convert things such as string to num
+//int num = Convert.ToInt32("12");
+//Console.WriteLine(num + 6);
+//Console.ReadLine();
