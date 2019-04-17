340e668dd4a0beec8cd8d957621ecc3abb5af0ac
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-16 22:49:10 -0400

    version13 added

--- learningCsharp/Program.cs
+++ learningCsharp/Program.cs
@@ -11,23 +11,30 @@ namespace learningCsharp
         static void Main(string[] args)
         {
 
-            Console.WriteLine(GetMax(20,10,99));
+            Console.Write("Enter a number: ");
+            //must convert string to double
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string oper = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (oper == "+"){
+                Console.WriteLine(num1 + num2);
+            }   else if(oper == "-"){
+                Console.WriteLine(num1 - num2);
+             }  else if(oper == "/"){
+                Console.WriteLine(num1 / num2);
+             }  else if(oper == "*"){
+                Console.WriteLine(num1 * num2);
+             }  else {
+                Console.WriteLine("Invalid Operator you stupid human!!!");
+             }
 
             Console.ReadLine();
 
-        }
-
-        static int GetMax(int num1, int num2, int num3){
-            int result;
-            if (num1 >= num2 && num1 >= num3)
-	         {
-                result = num1;
-	         }else if(num2 >= num1 && num2 >= num3){
-               result = num2;
-               } else {
-                   result = num3;
-               }
-            return result;
         }
     }
 }
